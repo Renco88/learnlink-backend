985afcfd396aec4e0ad2e62879e1a29082638630
Md Mahadi Hassan Renco <mdmahadihassanrenco@gmail.com>
2025-08-02 04:21:32 +0600

    Ready to show aa

--- server.js
+++ server.js
@@ -2,12 +2,8 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
-const http = require("http");
-const { Server } = require("socket.io");
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server, { cors: { origin: "*" } });
 
 app.use(cors());
 app.use(express.json());
@@ -31,4 +27,4 @@ app.use("/api/rooms", require("./src/routes/rooms"));
 app.use("/api/courses", require("./src/routes/courses"));
 
 const PORT = process.env.PORT || 5000;
-server.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
